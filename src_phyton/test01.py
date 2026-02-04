704e7f34c1f6c948995d670a7865d72fad3d6c3a
SANTI TOMAS <128708518+dopulus@users.noreply.github.com>
2026-02-04 10:50:36 +0100

    Delete src_phyton directory

--- src_phyton/test01.py
+++
@@ -1,8 +0,0 @@
-def main():
-    nome = input("Inserisci il tuo nome: ")
-    eta = int(input("Inserisci la tua età: "))
-
-    print(f"Ciao {nome}, hai {eta} anni.")
-
-if __name__ == "__main__":
-    main()
--- src_phyton/test03.py
+++
@@ -1,8 +0,0 @@
-def main():
-    n = int(input("Quanti numeri vuoi strivere? "))
-    
-    for i in range(1, n + 1):
-        print(f"Numero: {i}")
-
-if __name__ == "__main__":
-    main()
